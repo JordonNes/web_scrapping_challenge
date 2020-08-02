344cb0c1baab1c700fe02364d086f20cdb76a1a3
JordonNes <jordonnesbitt@live.com>
2020-08-02 10:54:41 -0700

    .

--- Mission_to_Mars/.ipynb_checkpoints/app.py
+++ Mission_to_Mars/.ipynb_checkpoints/app.py
@@ -1,20 +1,39 @@
 from flask import Flask, render_template, redirect
-app = Flask(__name__)
 from flask_pymongo import PyMongo
+import scrape_mars
 
-# @app.route('/')
-# def hello_world():
-#     return 'Hello, World!'@app.route('/')
+# Create an instance of Flask
+app = Flask(__name__)
 
+# Use PyMongo to establish Mongo connection
+mongo = PyMongo(app, uri="mongodb://localhost:27017/criselda_db")
+
+text = "Mission to Mars"
+# Route to render index.html template using data from Mongo
 @app.route("/")
-@app.route("/home")
 def home():
-    return render_template("index.html")
+
     # Find one record of data from the mongo database
-    # mars_info = mongo.db.collection.find_one()
+    mars_info = mongo.db.collection.find_one()
     # print(mars_info)
     # Return template and data
-    # , mars_info = mars_info, text = text 
+    return render_template("index.html", mars_info = mars_info, text = text)
+
+
+# Route that will trigger the scrape function
+@app.route("/scrape")
+def scrape():
+
+    # Run the scrape function
+    mars_info = scrape_mars.scrape_info()
+    #print(mars_info)
+
+    # Update the Mongo database using update and upsert=True
+    mongo.db.collection.update({}, mars_info, upsert=True)
+    
+    # Redirect back to home page
+    return redirect("/")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
